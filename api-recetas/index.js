527bfc9bf0ccdba9aa3f60c640b29ddcce2f7108
leombpunk <leomb_punk@hotmail.com>
2024-09-05 15:41:48 -0300

    agregada configuración de cors

--- api-recetas/index.js
+++ api-recetas/index.js
@@ -4,10 +4,22 @@ import cors from 'cors'
 import router from './routes/index.js'
 import { dbConexion } from './config/mysql.js'
 
+//config para los cors
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 dotenv.config()
 const app = express()
 const port = process.env.PORT
-app.use(cors())
+app.use(cors(/*corsOptions*/))
 app.use(express.json())
 app.use(router)
 app.listen(port, () => {
