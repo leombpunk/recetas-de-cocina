f92db3109bd5644afcc970a6a6164771369e5ae1
leombpunk <leomb_punk@hotmail.com>
2024-09-05 15:37:56 -0300

    agregado endpoint para checkear token

--- api-recetas/controllers/auth.js
+++ api-recetas/controllers/auth.js
@@ -1,63 +1,91 @@
-import Usuario from '../models/usuario.js'
-import { matchedData } from 'express-validator'
-import { httpError } from '../helpers/handleErrors.js'
-import { tokenSign } from '../helpers/generateToken.js'
-import { encrypt, compare } from '../helpers/handleBcrypt.js'
-import { sequelize } from '../config/mysql.js'
-import { handleResponse } from '../helpers/handleResponse.js'
+import { matchedData } from "express-validator"
+import { sequelize } from "../config/mysql.js"
+import Usuario from "../models/usuario.js"
+import { httpError } from "../helpers/handleErrors.js"
+import { tokenSign, verifyToken } from "../helpers/generateToken.js"
+import { encrypt, compare } from "../helpers/handleBcrypt.js"
+import { handleResponse } from "../helpers/handleResponse.js"
 
 const login = async (req, res) => {
-    try {
-        req = matchedData(req)
-        const { usuario, contrasena } = req
-        const result = await Usuario.getUsuarioByUserwPass(usuario)
-        console.log(result)
-        if (!result){
-            const status = 404
-            const message = 'User not found'
-            handleResponse(res, status, message)
-        }
-        
-        const checkPass = await compare(contrasena, result.contrasena)
-        if (checkPass) {
-            const token = await tokenSign(result)
-            const status = 200
-            const message = ''
-            const data = {...result.dataValues, token:token, contrasena: ''}
-            handleResponse(res, status, message, data)
-        }
-        else {
-            const status = 404
-            const message = 'Credentials not valid'
-            handleResponse(res, status, message)    
-        }
-    } catch (error) {
-        httpError(res, error)
+  try {
+    req = matchedData(req)
+    const { usuario, contrasena } = req
+    const result = await Usuario.getUsuarioByUserwPass(usuario)
+    console.log(result)
+    if (!result) {
+      const status = 404
+      const message = "User not found"
+      handleResponse(res, status, message)
     }
+
+    const checkPass = await compare(contrasena, result.contrasena)
+    if (checkPass) {
+      const token = await tokenSign(result)
+      const status = 200
+      const message = ""
+      delete result.dataValues.contrasena
+      const data = { ...result.dataValues, token: token }
+      handleResponse(res, status, message, data)
+    } else {
+      const status = 404
+      const message = "Credentials not valid"
+      handleResponse(res, status, message)
+    }
+  } catch (error) {
+    httpError(res, error)
+  }
 }
 
 const registro = async (req, res) => {
-    try {
-        req = matchedData(req)
-        const { nombres, apellidos, usuario, contrasena, mail } = req
-        const contraHash = await encrypt(contrasena)
-        const result = await sequelize.transaction(async (t) => {
-            const { id } = await Usuario.create({ nombres, apellidos, usuario, contrasena: contraHash, mail }, { transaction: t })
-            return await Usuario.findOne({ where: { id: id }})
-        })
-        const status = 201
-        const message = 'User registrer done'
-        const data = result
-        handleResponse(res, status, message, data)
-    } catch (error) {
-        httpError(res, error)
-    }
+  try {
+    req = matchedData(req)
+    const { nombres, apellidos, usuario, contrasena, mail } = req
+    const contraHash = await encrypt(contrasena)
+    const result = await sequelize.transaction(async (t) => {
+      const { id } = await Usuario.create(
+        { nombres, apellidos, usuario, contrasena: contraHash, mail },
+        { transaction: t }
+      )
+      return await Usuario.findOne({ where: { id: id } })
+    })
+    const status = 201
+    const message = "User registrer done"
+    const data = result
+    handleResponse(res, status, message, data)
+  } catch (error) {
+    httpError(res, error)
+  }
 }
 
-const registroGoogle = async (req, res) => {} 
+const registroGoogle = async (req, res) => {}
+
+const refreshUserData = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ").pop()
+    const tokenData = await verifyToken(token)
+
+    const result = await Usuario.findOne({
+      where: { id: tokenData.id, usuario: tokenData.usuario },
+    })
+    console.log(result)
+    if (!result) {
+      const status = 404
+      const message = "User not found"
+      handleResponse(res, status, message)
+    } else {
+      const newToken = await tokenSign(result)
+      const status = 200
+      const message = ""
+      const data = { ...result.dataValues, token: newToken }
+      handleResponse(res, status, message, data)
+    }
+  } catch (error) {
+    httpError(res, error)
+  }
+}
 
 const logout = async (req, res) => {
-    // nones
+  // nones
 }
 
-export { login, logout, registro }
+export { login, logout, registro, refreshUserData }
--- api-recetas/routes/auth.js
+++ api-recetas/routes/auth.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { validateAuth, validateRegistro } from '../validators/auth.js'
-import { login, registro, logout } from '../controllers/auth.js'
+import { login, registro, logout, refreshUserData } from '../controllers/auth.js'
+import { checkAuth } from '../middlewares/auth.js'
 
 const router = Router()
 
 router.post('/login', validateAuth, login) //TESTEADO
 router.post('/registro', validateRegistro, registro) //TESTEAR ENDPOINT
+router.post('/verifyToken', checkAuth, refreshUserData) //verficar si el token no expiro y si el usuario existe
 // router.post('/logout') //ELIMINAR
 
 export { router }
