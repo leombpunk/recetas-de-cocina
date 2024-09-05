736c5efe9a49112cbf3e5d46ee7293824f130b85
leombpunk <leomb_punk@hotmail.com>
2024-09-05 15:42:02 -0300

    Update auth.js

--- api-recetas/middlewares/auth.js
+++ api-recetas/middlewares/auth.js
@@ -1,44 +1,54 @@
 import { verifyToken } from "../helpers/generateToken.js"
 
 const checkAuth = async (request, response, next) => {
-    try {
-        const token = request.headers.authorization.split(' ').pop()
-        const tokenData = await verifyToken(token)
-        if(tokenData.usuario) {
-            next()
-        } else{
-            response.status(401)
-            response.send({ error: 'No posees permisos suficientes para realizar esta operacion' })
-        }
-    } catch (error) {
-        console.log('/* checkAuth middleware (catch) */')
-        console.log(error)
-        response.status(401)
-        response.send({ message: 'No posees permisos suficientes para realizar esta operacion', error: error })
+  try {
+    const token = request.headers.authorization.split(" ").pop()
+    const tokenData = await verifyToken(token)
+    // console.log({tokenData: tokenData})
+    if (tokenData.usuario) {
+      next()
+    } else {
+      response.status(401)
+      response.send({
+        error: "No posees permisos suficientes para realizar esta operacion",
+      })
     }
+  } catch (error) {
+    console.log("/* checkAuth middleware (catch) */")
+    console.log(error)
+    response.status(401)
+    response.send({
+      message: "No posees permisos suficientes para realizar esta operacion",
+      error: error,
+    })
+  }
 }
 
 // me gustaria hacerlo de otra manera
 const checkCoherence = async (request, response, next) => {
-    try {
-        const token = request.headers.authorization.split(' ').pop()
-        const tokenData = await verifyToken(token)
-        // const data = request.body
-        const id = request.params.id || request.body.idUsuario || request.body.id //testear esto
-        // console.log({ id: id })
-        if (tokenData.id == id) {
-            next()
-        } else {
-            response.status(401)
-            response.send({ error: 'No posees permisos suficientes para realizar esta operacion' })
-        }
-    } catch (error) {
-        console.log('/* checkCoherence middleware (catch) */')
-        console.log(error)
-        response.status(401)
-        response.send({ message: 'No posees permisos suficientes para realizar esta operacion', error: error })
+  try {
+    const token = request.headers.authorization.split(" ").pop()
+    const tokenData = await verifyToken(token)
+    // const data = request.body
+    const id = request.params.id || request.body.idUsuario || request.body.id //testear esto
+    // console.log({ id: id })
+    if (tokenData.id == id) {
+      next()
+    } else {
+      response.status(401)
+      response.send({
+        error: "No posees permisos suficientes para realizar esta operacion",
+      })
     }
+  } catch (error) {
+    console.log("/* checkCoherence middleware (catch) */")
+    console.log(error)
+    response.status(401)
+    response.send({
+      message: "No posees permisos suficientes para realizar esta operacion",
+      error: error,
+    })
+  }
 }
-
 
 export { checkAuth, checkCoherence }
