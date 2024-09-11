058781bb781ca7f6ba2b48a5b659f3a9b6e2c014
leombpunk <leomb_punk@hotmail.com>
2024-09-11 16:40:04 -0300

    implementando hook

--- recipe-book-app/src/pages/RecipePage.jsx
+++ recipe-book-app/src/pages/RecipePage.jsx
@@ -1,27 +1,33 @@
 import { useEffect } from "react"
 import RecipeForm from "../components/recipe-form/RecipeForm"
 import { useContextNotification } from "../providers/NotificationProvider"
-import { saveRecipeLocal, getRecipeLocal, recipeStruct } from "../utils/RecipeLocal"
+import {
+  saveRecipeLocal,
+  getRecipeLocal,
+  recipeStruct,
+} from "../utils/RecipeLocal"
 import useRecipe from "../hooks/useRecipe"
+import Loader from "../components/loader/Loader"
 
-const RecipePage = ({idRecipe}) => {
-  //al iniciar el componente verificar si existe algun dato en localstorage/server
+const RecipePage = ({ idRecipe }) => {
   const recipeLocal = getRecipeLocal()
   // console.log(recipeLocal)
   //si hay, cargar los datos en el formulario
   //si no hay nada crear el "dato" en localstorage/server
   const { addNotification } = useContextNotification()
-  const { recipe, createRecipe, updateRecipe, deleteRecipe, getRecipe } = useRecipe()
+  const { recipe, createRecipe, updateRecipe, deleteRecipe, getRecipe } =
+    useRecipe()
 
+  //al iniciar el componente verificar si existe algun dato en localstorage/server
   useEffect(() => {
     if (!recipeLocal) {
       createRecipe()
-    } 
-    else {
-      console.log(recipeLocal)
-      getRecipe(recipeLocal.id)
+    } else {
+      // console.log(recipeLocal)
+      getRecipe(idRecipe || recipeLocal.id)
     }
-  },[])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
     saveRecipeLocal(recipe)
@@ -31,7 +37,17 @@ const RecipePage = ({idRecipe}) => {
     <>
       <section className='bg-orange-300'>
         <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
-          <RecipeForm title="Nueva Receta" handleNotification={addNotification} data={recipe} />
+          {recipe ? (
+            <RecipeForm
+              title='Nueva Receta'
+              handleNotification={addNotification}
+              data={recipe}
+              handleSave={updateRecipe}
+              handleDelete={deleteRecipe}
+            />
+          ) : (
+            <Loader />
+          )}
         </div>
       </section>
     </>
