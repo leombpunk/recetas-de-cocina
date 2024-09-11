8d4eb55d42f88f285228316e9906c99384cba0bb
leombpunk <leomb_punk@hotmail.com>
2024-09-11 16:38:43 -0300

    resolver

---
+++ recipe-book-app/src/utils/RecipeResolver.js
@@ -0,0 +1,19 @@
+import * as yup from "yup"
+
+const recipeSchema = yup.object({
+  titulo: yup.string().required("Ponle un titulo a tu receta"),
+  detalle: yup.string(),
+  duracion: yup.string(),
+  comensales: yup.string(),
+  visibilidad: yup.number(),
+  ingredientes: yup.array().of(yup.object({
+    ingrediente: yup.string().required()
+  })),
+  pasos: yup.array().of(yup.object({
+    paso: yup.string().required(),
+    imagen: yup.string().url().notRequired()
+  })),
+  imagen: yup.string()
+})
+
+export default recipeSchema
