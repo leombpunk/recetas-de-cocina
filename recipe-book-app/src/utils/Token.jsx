4b657519508b0a210eb5808820c76e8f69b01b50
leombpunk <leomb_punk@hotmail.com>
2024-09-13 21:06:27 -0300

    Update Token.jsx

--- recipe-book-app/src/utils/Token.jsx
+++ recipe-book-app/src/utils/Token.jsx
@@ -1,35 +1,35 @@
 const getToken = () => {
-    try {
-        const token = localStorage.getItem('RecipeAppToken')
-        if (token){
-            // const user = JSON.parse(token)
-            // return `Bearer ${user.token}`
-            return `Bearer ${token}`
-        }
-    } catch (error) {
-        return { errors: error }
+  try {
+    const token = localStorage.getItem("RecipeAppToken")
+    if (token) {
+      // const user = JSON.parse(token)
+      // return `Bearer ${user.token}`
+      return `Bearer ${token}`
     }
+  } catch (error) {
+    return { errors: error }
+  }
 }
 
 const setToken = (userToken) => {
-    try {
-        // localStorage.setItem('RecipeAppToken', JSON.stringify(userToken))
-        localStorage.setItem('RecipeAppToken', userToken)
-        return localStorage.getItem('RecipeAppToken')
-    } catch (error) {
-        console.log("Error", error)
-        return { 'error': error }
-    }
+  try {
+    // localStorage.setItem('RecipeAppToken', JSON.stringify(userToken))
+    localStorage.setItem("RecipeAppToken", userToken)
+    return localStorage.getItem("RecipeAppToken")
+  } catch (error) {
+    console.log("Error", error)
+    return { error: error }
+  }
 }
 
 const deleteToken = () => {
-    try {
-        localStorage.removeItem('RecipeAppToken')
-        return { 'message': 'ok' }
-    } catch (error) {
-        console.log("Error", error)
-        return { 'error': error }
-    }
+  try {
+    localStorage.removeItem("RecipeAppToken")
+    return { message: "ok" }
+  } catch (error) {
+    console.log("Error", error)
+    return { error: error }
+  }
 }
 
 export { getToken, setToken, deleteToken }
