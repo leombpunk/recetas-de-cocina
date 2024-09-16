3a72bf0e3d3ff4fa3bf1b748144796b0827c54d4
leombpunk <leomb_punk@hotmail.com>
2024-09-16 18:38:00 -0300

    Update recetas.js

--- api-recetas/validators/recetas.js
+++ api-recetas/validators/recetas.js
@@ -33,23 +33,9 @@ const validateCreate = [
     .isNumeric()
     .isLength({ max: 1 })
     .optional(),
-  check("ingredientes.*.ingrediente")
+  check("ingredientes.*.name")
     .exists()
     .isLength({ max: 50 })
-    .optional(),
-  check("ingredientes.*.cantidad")
-    .exists()
-    .isLength({ max: 20 })
-    .optional(),
-  check("ingredientes.*.orden")
-    .exists()
-    .isNumeric()
-    .isLength({ max: 2 })
-    .optional(),
-  check("pasos.*.orden")
-    .exists()
-    .isNumeric()
-    .isLength({ max: 2 })
     .optional(),
   check("pasos.*.paso")
     .exists()
@@ -76,27 +62,18 @@ const validateReceta = [
   check("idUsuario")
     .exists().isNumeric().isLength({ max: 11 }).not().isEmpty(),
   check("imagen")
-    .exists().isLength({ max: 100 }),
+    .exists().isLength({ max: 100 }).not().isEmpty(),
   check("visibilidad")
     .exists()
     .not()
     .isEmpty()
     .isNumeric()
     .isLength({ max: 1 }),
-  check("ingredientes.*.ingrediente")
+  check("ingredientes.*.name")
     .exists()
     .isLength({ max: 50 })
     .not()
     .isEmpty(),
-  check("ingredientes.*.cantidad")
-    .exists()
-    .isLength({ max: 20 })
-    .not()
-    .isEmpty(),
-  check("ingredientes.*.orden")
-    .exists().isNumeric().isLength({ max: 2 }),
-  check("pasos.*.orden")
-    .exists().isNumeric().isLength({ max: 2 }),
   check("pasos.*.paso")
     .exists().isLength({ max: 500 }).not().isEmpty(),
   check("pasos.*.imagen")
