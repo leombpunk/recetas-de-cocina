f1f1d07620aa38bb3e518d3f063e38da1b3516cb
leombpunk <leomb_punk@hotmail.com>
2024-09-16 19:12:55 -0300

    Update Header.jsx

--- recipe-book-app/src/components/header/Header.jsx
+++ recipe-book-app/src/components/header/Header.jsx
@@ -11,12 +11,19 @@ function classNames(...classes) {
 
 const Header = () => {
   const navigate = useNavigate()
-  const { handleLogout } = useContextUser()
+  const { user, handleLogout } = useContextUser()
+
   const handleClickEndSesion = (e) => {
     e.preventDefault()
     handleLogout()
+    navigate(NavigationRoutes.Home)
+  }
+
+  const handleClickSignIn = (e) => {
+    e.preventDefault()
     navigate(NavigationRoutes.Login)
   }
+
   return (
     <Disclosure as='nav' className='bg-orange-500'>
       {({ open }) => (
@@ -56,7 +63,7 @@ const Header = () => {
                   </div>
                 </div>
               </div>
-              <div className='inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto ml-2.5 sm:ml-6 sm:pr-0'>
+              <div className='inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto ml-2.5 sm:ml-6 sm:pr-0 gap-2'>
                 <button
                   type='button'
                   className='relative rounded-full hover:scale-125 duration-500 bg-orange-500 p-1 text-gray-900 hover:text-black focus:outline-none focus:ring-2 focus:ring-orange-700 focus:ring-offset-2 focus:ring-offset-orange-800'
@@ -67,84 +74,108 @@ const Header = () => {
                 </button>
 
                 {/* Profile dropdown */}
-                <Menu as='div' className='relative ml-3'>
-                  <div>
-                    <Menu.Button className='relative flex rounded-full hover:scale-125 duration-500 bg-orange-500 text-sm focus:outline-none focus:ring-2 focus:ring-orange-700 focus:ring-offset-2 focus:ring-offset-orange-800'>
-                      <span className='absolute -inset-1.5' />
-                      <span className='sr-only'>Abrir menu de usuario</span>
-                      <img
-                        className='h-8 w-8 rounded-full'
-                        src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                        alt=''
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter='transition ease-out duration-100'
-                    enterFrom='transform opacity-0 scale-95'
-                    enterTo='transform opacity-100 scale-100'
-                    leave='transition ease-in duration-75'
-                    leaveFrom='transform opacity-100 scale-100'
-                    leaveTo='transform opacity-0 scale-95'
+                {user ? (
+                  <Menu as='div' className='relative ml-3'>
+                    <div>
+                      <Menu.Button className='relative flex rounded-full hover:scale-110 duration-500 bg-orange-500 text-sm focus:outline-none focus:ring-2 focus:ring-orange-700 focus:ring-offset-2 focus:ring-offset-orange-800'>
+                        <span className='absolute -inset-1.5' />
+                        <span className='sr-only'>Abrir menu de usuario</span>
+                        <img
+                          className='h-8 w-8 rounded-full'
+                          src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                          alt=''
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter='transition ease-out duration-100'
+                      enterFrom='transform opacity-0 scale-95'
+                      enterTo='transform opacity-100 scale-100'
+                      leave='transition ease-in duration-75'
+                      leaveFrom='transform opacity-100 scale-100'
+                      leaveTo='transform opacity-0 scale-95'
+                    >
+                      <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <span
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Nombre de usuario
+                            </span>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                              to={NavigationRoutes.Profile}
+                            >
+                              Perfil
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to={NavigationRoutes.Recipes}
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Mis Recetas
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to={NavigationRoutes.Recipe}
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Nueva Receta
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              type='button'
+                              onClick={(e) => handleClickEndSesion(e)}
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block w-full px-4 py-2 text-sm text-left text-gray-700"
+                              )}
+                            >
+                              Cerrar sesión
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                ) : (
+                  <button
+                    type='button'
+                    onClick={(e) => handleClickSignIn(e)}
+                    className='relative rounded-xl py-2 px-3 hover:scale-110 duration-500 bg-orange-400 text-gray-900 hover:text-black font-semibold shadow-black/50 hover:shadow-md'
                   >
-                    <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                            to={NavigationRoutes.Profile}
-                          >
-                            Perfil
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to={NavigationRoutes.Recipes}
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Mis Recetas
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to={NavigationRoutes.Recipe}
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Nueva Receta
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <button
-                            type="button"
-                            onClick={(e) => handleClickEndSesion(e)}
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block w-full px-4 py-2 text-sm text-left text-gray-700"
-                            )}
-                          >
-                            Cerrar sesión
-                          </button>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                    <span className='absolute -inset-1.5' />
+                    <span className='sr-only'>Iniciar sesión</span>
+                    Conectate
+                  </button>
+                )}
               </div>
             </div>
           </div>
