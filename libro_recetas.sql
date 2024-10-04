7521dee3875c6db4760247605184175c3061bcee
leombpunk <leomb_punk@hotmail.com>
2024-10-04 20:35:28 -0300

    base de datos actualizada

--- libro_recetas.sql
+++ libro_recetas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2024 a las 22:42:44
+-- Tiempo de generación: 05-10-2024 a las 01:32:31
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -53,7 +53,13 @@ INSERT INTO `archivos` (`id`, `idUsuario`, `imagen`, `createAt`, `deleteAt`) VAL
 (8, 1, 'file-1726510198561.jpg', '2024-09-16 18:09:58', NULL),
 (9, 1, 'file-1726510766090.jpg', '2024-09-16 15:19:26', NULL),
 (10, 1, 'file-1726510957757.jpg', '2024-09-16 15:22:37', NULL),
-(11, 1, 'file-1726512526382.webp', '2024-09-16 15:48:46', NULL);
+(11, 1, 'file-1726512526382.webp', '2024-09-16 15:48:46', NULL),
+(12, 1, 'file-1726590848211.PNG', '2024-09-17 13:34:08', NULL),
+(13, 1, 'file-1726590994336.PNG', '2024-09-17 13:36:34', NULL),
+(14, 1, 'file-1726591483365.PNG', '2024-09-17 13:44:43', '2024-09-17 15:41:57'),
+(15, 1, 'file-1726592221361.PNG', '2024-09-17 13:57:01', '2024-09-17 15:42:29'),
+(16, 1, 'file-1726600183413.PNG', '2024-09-17 16:09:43', '2024-09-19 14:07:46'),
+(17, 1, 'file-1726683473740.jpg', '2024-09-18 15:17:53', '2024-09-19 14:07:46');
 
 -- --------------------------------------------------------
 
@@ -67,7 +73,22 @@ CREATE TABLE `comentarios` (
   `idReceta` int(11) UNSIGNED NOT NULL,
   `idUsuario` int(11) UNSIGNED NOT NULL,
   `comentario` varchar(1000) NOT NULL,
-  `fecha` datetime NOT NULL
+  `createAt` datetime NOT NULL,
+  `isReply` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `guardadas`
+--
+
+DROP TABLE IF EXISTS `guardadas`;
+CREATE TABLE `guardadas` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `idUsuario` int(11) UNSIGNED NOT NULL,
+  `idReceta` int(11) UNSIGNED NOT NULL,
+  `createAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -80,9 +101,16 @@ DROP TABLE IF EXISTS `likes`;
 CREATE TABLE `likes` (
   `id` int(11) UNSIGNED NOT NULL,
   `idUsuario` int(11) UNSIGNED NOT NULL,
-  `idReceta` int(1) UNSIGNED NOT NULL,
-  `fecha` date NOT NULL
+  `idReceta` int(11) UNSIGNED NOT NULL,
+  `createAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `likes`
+--
+
+INSERT INTO `likes` (`id`, `idUsuario`, `idReceta`, `createAt`) VALUES
+(1, 1, 5, '2024-10-04 19:56:38');
 
 -- --------------------------------------------------------
 
@@ -102,37 +130,37 @@ CREATE TABLE `recetas` (
   `duracion` varchar(20) NOT NULL,
   `checked` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '0=no verificado 1=verificado',
   `ingredientes` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '[{"name":""}]',
-  `pasos` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '[{"paso":"","imagen":""}]'
+  `pasos` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '[{"paso":"","imagen":""}]',
+  `createAt` datetime DEFAULT NULL,
+  `updateAt` datetime DEFAULT NULL,
+  `deleteAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `recetas`
 --
 
-INSERT INTO `recetas` (`id`, `idUsuario`, `titulo`, `detalle`, `imagen`, `visibilidad`, `comensales`, `duracion`, `checked`, `ingredientes`, `pasos`) VALUES
-(1, 1, 'pan con manteca', 'primero compras el pan, lo cortas con un cuchillo, abrís la manteca y la untas sobre el pan', '', 1, '', '', 1, '[]', '[]'),
-(2, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', 'file-1688405770766..jpg', 0, '', '', 1, '[]', '[]'),
-(3, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(4, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(5, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(6, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(7, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(8, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(9, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(10, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(12, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(13, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]'),
-(25, 1, 'receta test', 'test', '', 1, '2 weones', '2 horas', 1, '[]', '[]'),
-(26, 1, 'bizcochuelo marmolado test', 'mezcla todo como venga ameo', '', 1, '5 personas', '1 hora', 1, '[]', '[]'),
-(28, 1, 'te con leche', 'es un te con leche pa', '', 1, '2 personas', '15 minutos', 1, '[]', '[]'),
-(29, 1, 'bizcochuelo marmolado test', 'mezcla todo como venga ameo', '', 1, '5 personas', '1 hora', 1, '[]', '[]'),
-(30, 1, 'test middleware', '', '', 0, '', '', 1, '[]', '[]'),
-(31, 1, 'test middleware', '', '', 0, '', '', 1, '[]', '[]'),
-(32, 1, 'test middleware', '', '', 0, '', '', 1, '[]', '[]'),
-(33, 1, 'test middleware', '', '', 0, '', '', 1, '[]', '[]'),
-(34, 1, 'test middleware', '', '', 0, '', '', 1, '[]', '[]'),
-(35, 1, 'salsa bolognesa', '', '', 0, '', '', 1, '[\"carne picada\"]', '[{\"paso\":\"\",\"imagen\":\"\"}]'),
-(36, 1, 'test middleware', 'mezcla todo como venga ameo', '', 1, '5 personas', '1 hora', 0, '[{\"name\":\"15 cucharadas de azúcar\"},{\"name\":\"15 cucharadas de harina leudante\"},{\"name\":\"1 huevo\"},{\"name\":\"100 gramos de margarina repostera\"},{\"name\":\"3 cucharaditas de vainilla líquida\"},{\"name\":\"5 cucharadas de chocolate en polvo\"}]', '[{\"paso\":\"paso2 blabla blabla\",\"imagen\":\"\"},{\"paso\":\"paso1 blabla blabla\",\"imagen\":\"\"}]');
+INSERT INTO `recetas` (`id`, `idUsuario`, `titulo`, `detalle`, `imagen`, `visibilidad`, `comensales`, `duracion`, `checked`, `ingredientes`, `pasos`, `createAt`, `updateAt`, `deleteAt`) VALUES
+(1, 1, 'pan con manteca', 'primero compras el pan, lo cortas con un cuchillo, abrís la manteca y la untas sobre el pan', '', 1, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(2, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', 'file-1726512404785.webp', 1, '5 persianas', '30 minutos', 1, '[{\"name\":\"1kg de harina\"},{\"name\":\"50gr de leudante\"}]', '[{\"paso\":\"merzclar las weas en un recipiente limpio\",\"imagen\":\"\"}]', NULL, NULL, NULL),
+(3, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 1, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(4, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(5, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(6, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(7, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(8, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(9, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(10, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(12, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(13, 1, 'bizcochuelo marmolado', 'mezcla todo como venga', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(25, 1, 'receta test', 'test', '', 1, '2 weones', '2 horas', 1, '[]', '[]', NULL, NULL, NULL),
+(26, 1, 'bizcochuelo marmolado test', 'mezcla todo como venga ameo', '', 1, '5 personas', '1 hora', 1, '[]', '[]', NULL, NULL, NULL),
+(28, 1, 'te con leche', 'es un te con leche pa', '', 1, '2 personas', '15 minutos', 1, '[]', '[]', NULL, NULL, NULL),
+(29, 1, 'bizcochuelo marmolado test', 'mezcla todo como venga ameo', '', 1, '5 personas', '1 hora', 1, '[]', '[]', NULL, NULL, NULL),
+(30, 1, 'test middleware', '', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(31, 1, 'test middleware', '', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(32, 1, 'test middleware', '', '', 0, '', '', 1, '[]', '[]', NULL, NULL, NULL),
+(33, 1, 'test middleware', '', '', 0, '', '', 0, '[]', '[]', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -148,18 +176,19 @@ CREATE TABLE `usuarios` (
   `usuario` varchar(16) NOT NULL,
   `contrasena` varchar(255) NOT NULL COMMENT 'pass: usuario123',
   `mail` varchar(50) NOT NULL,
-  `imagen` varchar(100) DEFAULT NULL
+  `imagen` varchar(100) DEFAULT NULL,
+  `createAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `apellidos`, `nombres`, `usuario`, `contrasena`, `mail`, `imagen`) VALUES
-(1, '', '', 'usuario123', '$2a$10$733Dv09gafNx74PombmlW.9CtTu2HolqqTxJ3bW2lRxLkTbN5vYEu', 'mail_123@update.com', NULL),
-(2, '', '', 'usuario456', '$2a$10$733Dv09gafNx74PombmlW.9CtTu2HolqqTxJ3bW2lRxLkTbN5vYEu', 'usu456@ario.com', NULL),
-(3, '', '', 'usuario124', '$2a$10$733Dv09gafNx74PombmlW.9CtTu2HolqqTxJ3bW2lRxLkTbN5vYEu', 'mail@testeo.com', NULL),
-(5, '', '', 'usuarioTest', '$2a$10$DiZim3xEPrsAmcuZuqx6pepMxXDfxwf8Q7aFkscnKZ9WENCR8LI3i', 'mail_equisde@testeo.com', NULL);
+INSERT INTO `usuarios` (`id`, `apellidos`, `nombres`, `usuario`, `contrasena`, `mail`, `imagen`, `createAt`) VALUES
+(1, '', '', 'usuario123', '$2a$10$733Dv09gafNx74PombmlW.9CtTu2HolqqTxJ3bW2lRxLkTbN5vYEu', 'mail_123@update.com', NULL, NULL),
+(2, '', '', 'usuario456', '$2a$10$733Dv09gafNx74PombmlW.9CtTu2HolqqTxJ3bW2lRxLkTbN5vYEu', 'usu456@ario.com', NULL, NULL),
+(3, '', '', 'usuario124', '$2a$10$733Dv09gafNx74PombmlW.9CtTu2HolqqTxJ3bW2lRxLkTbN5vYEu', 'mail@testeo.com', NULL, NULL),
+(5, '', '', 'usuarioTest', '$2a$10$DiZim3xEPrsAmcuZuqx6pepMxXDfxwf8Q7aFkscnKZ9WENCR8LI3i', 'mail_equisde@testeo.com', NULL, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -179,10 +208,20 @@ ALTER TABLE `comentarios`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `guardadas`
+--
+ALTER TABLE `guardadas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_usuario_receta` (`idUsuario`,`idReceta`) USING BTREE;
+
+--
 -- Indices de la tabla `likes`
 --
 ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_usuario_receta` (`idUsuario`,`idReceta`) USING BTREE,
+  ADD KEY `ix_idUsuario` (`idUsuario`) USING BTREE,
+  ADD KEY `ix_idReceta` (`idReceta`) USING BTREE;
 
 --
 -- Indices de la tabla `recetas`
@@ -208,7 +247,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `archivos`
 --
 ALTER TABLE `archivos`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `comentarios`
@@ -217,10 +256,16 @@ ALTER TABLE `comentarios`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `guardadas`
+--
+ALTER TABLE `guardadas`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `recetas`
@@ -243,6 +288,13 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `archivos`
   ADD CONSTRAINT `fk_usuario2` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `fk_recetas_likes` FOREIGN KEY (`idReceta`) REFERENCES `recetas` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuarios_likes` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`) ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `recetas`
