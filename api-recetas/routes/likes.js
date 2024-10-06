0dc18438c64e26eed96e746d5fd9bf8b59fd1446
leombpunk <leomb_punk@hotmail.com>
2024-10-06 16:43:54 -0300

    detalle de likes ahora requiere autorización

--- api-recetas/routes/likes.js
+++ api-recetas/routes/likes.js
@@ -10,8 +10,8 @@ import {
 const router = Router()
 
 router.get("/:id", getLikes) //todos los likes de una receta (cantidad)
-router.get("/details/:id", getLikesDetails) //lista de usuarios que dieron like a una receta
 
+router.get("/details/:id", checkAuth, getLikesDetails) //lista de usuarios que dieron like a una receta
 router.post("/:id", checkAuth, createLike) //agregar like a receta
 router.delete("/:id", checkAuth, deteleLike) //borrar like a receta
 
