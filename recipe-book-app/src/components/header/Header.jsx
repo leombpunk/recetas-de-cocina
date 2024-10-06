bae3080d9fb38d924cf28584f005e87d81dbae6d
leombpunk <leomb_punk@hotmail.com>
2024-10-06 18:03:48 -0300

    Update Header.jsx

--- recipe-book-app/src/components/header/Header.jsx
+++ recipe-book-app/src/components/header/Header.jsx
@@ -110,11 +110,6 @@ const Header = () => {
                         ) : (
                           <UserCircleIcon className='h-8 w-8 rounded-full text-gray-900 hover:text-black' />
                         )}
-                        {/* <img
-                          className='h-8 w-8 rounded-full'
-                          src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                          alt=''
-                        /> */}
                       </Menu.Button>
                     </div>
                     <Transition
