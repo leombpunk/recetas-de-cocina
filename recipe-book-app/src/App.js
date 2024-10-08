e465a1c90e001cf7f45575c6ce53e85dae9ea82b
leombpunk <leomb_punk@hotmail.com>
2024-10-07 21:01:16 -0300

    agregado fontawesome

--- recipe-book-app/src/App.js
+++ recipe-book-app/src/App.js
@@ -1,12 +1,34 @@
-import './App.css'
-import 'react-toastify/dist/ReactToastify.css'
-import { BrowserRouter, Route, Routes } from  'react-router-dom'
-import AppContainer from './components/AppContainer'
-import NavigationRoutes from './utils/NavigationRoutes'
-import { UserProvider } from './providers/UserProvider'
-import { NotificationProvider } from './providers/NotificationProvider'
-import { HomePage, NotFoundPage, LoginPage, ProfilePage, RecipePage, RecipesPage, RegisterPage, SearchPage, RecipePublicPage } from './pages/index'
-import ProtectedRoutes from './components/ProtectedRoutes'
+import "./App.css"
+import "react-toastify/dist/ReactToastify.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import AppContainer from "./components/AppContainer"
+import NavigationRoutes from "./utils/NavigationRoutes"
+import { UserProvider } from "./providers/UserProvider"
+import { NotificationProvider } from "./providers/NotificationProvider"
+import {
+  HomePage,
+  NotFoundPage,
+  LoginPage,
+  ProfilePage,
+  RecipePage,
+  RecipesPage,
+  RegisterPage,
+  SearchPage,
+  RecipePublicPage,
+} from "./pages/index"
+import ProtectedRoutes from "./components/ProtectedRoutes"
+//iconos fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core"
+import {
+  faFacebook,
+  faWhatsapp,
+  faInstagram,
+  faXTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope, faLink } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faFacebook, faWhatsapp, faInstagram, faXTwitter, faEnvelope, faLink, faLinkedin)
 
 const App = () => {
   return (
@@ -14,18 +36,39 @@ const App = () => {
       <NotificationProvider>
         <BrowserRouter>
           <Routes>
-            <Route element={<ProtectedRoutes />} >
+            <Route element={<ProtectedRoutes />}>
               <Route path={NavigationRoutes.Login} element={<LoginPage />} />
-              <Route path={NavigationRoutes.Register} element={<RegisterPage />} />
-                <Route element={<AppContainer />}>
-                  <Route path={NavigationRoutes.Home} element={<HomePage />} />
-                  <Route path={NavigationRoutes.Profile} element={<ProfilePage />} />
-                  <Route path={NavigationRoutes.RecipeCreate} element={<RecipePage />} />
-                  <Route path={NavigationRoutes.Recipe} element={<RecipePage />} />
-                  <Route path={NavigationRoutes.Recipes} element={<RecipesPage />} />
-                  <Route path={NavigationRoutes.Search} element={<SearchPage />} />
-                  <Route path={NavigationRoutes.SearchRecipe} element={<RecipePublicPage />} />
-                  <Route path={"*"} element={<NotFoundPage/>}/>
+              <Route
+                path={NavigationRoutes.Register}
+                element={<RegisterPage />}
+              />
+              <Route element={<AppContainer />}>
+                <Route path={NavigationRoutes.Home} element={<HomePage />} />
+                <Route
+                  path={NavigationRoutes.Profile}
+                  element={<ProfilePage />}
+                />
+                <Route
+                  path={NavigationRoutes.RecipeCreate}
+                  element={<RecipePage />}
+                />
+                <Route
+                  path={NavigationRoutes.Recipe}
+                  element={<RecipePage />}
+                />
+                <Route
+                  path={NavigationRoutes.Recipes}
+                  element={<RecipesPage />}
+                />
+                <Route
+                  path={NavigationRoutes.Search}
+                  element={<SearchPage />}
+                />
+                <Route
+                  path={NavigationRoutes.SearchRecipe}
+                  element={<RecipePublicPage />}
+                />
+                <Route path={"*"} element={<NotFoundPage />} />
               </Route>
             </Route>
           </Routes>
