e4a23f83b499cc12e9e288a24884d98cb5126d93
leombpunk <leomb_punk@hotmail.com>
2025-02-08 14:13:51 -0300

    Update Footer.jsx

--- recipe-book-app/src/components/footer/Footer.jsx
+++ recipe-book-app/src/components/footer/Footer.jsx
@@ -3,33 +3,33 @@ const Footer = () => {
         <>
             <footer>
                 <div className=" bg-orange-700">
-                    <div className="max-w-2xl mx-auto text-black py-10">
+                    <div className="max-w-4xl mx-auto text-white font-semibold py-10">
                         <div className="text-center">
-                            <h3 className="text-3xl mb-3">Descargate la App</h3>
-                            <p> Lleva tus recetas donde quieras.</p>
+                            <h3 className="text-4xl mb-3">Descargate la App</h3>
+                            <p className="text-lg"> Lleva tus recetas donde quieras.</p>
                             <div className="flex justify-center my-10">
-                                <div className="flex items-center border border-black  rounded-lg px-4 py-2 w-52 mx-2">
-                                    <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" className="w-7 md:w-8" alt="descarga la app desde la play store"/>
+                                <a href="#app" className="flex items-center border border-gray-100 rounded-lg px-4 py-2 w-60 mx-2 bg-orange-600 shadow-black/70 hover:shadow-lg">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" className="w-7 md:w-10" alt="descarga la app desde la play store"/>
                                     <div className="text-left ml-3">
-                                        <p className='text-xs text-gray-900'>Descarga</p>
-                                        <p className="text-sm md:text-base"> Google Play Store </p>
+                                        <p className='text-xs text-gray-100'>Descarga</p>
+                                        <p className="text-sm md:text-lg"> Google Play Store </p>
                                     </div>
-                                </div>
-                                <div className="flex items-center border border-black rounded-lg px-4 py-2 w-52 mx-2">
-                                    <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" className="w-7 md:w-8" alt="descarga la app desde la apple store"/>
+                                </a>
+                                <a href="#app" className="flex items-center border border-gray-100 rounded-lg px-4 py-2 w-60 mx-2 bg-orange-600 shadow-black/70 hover:shadow-lg">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" className="w-7 md:w-10" alt="descarga la app desde la apple store"/>
                                     <div className="text-left ml-3">
-                                        <p className='text-xs text-gray-900'>Descarga</p>
-                                        <p className="text-sm md:text-base"> Apple Store </p>
+                                        <p className='text-xs text-gray-100'>Descarga</p>
+                                        <p className="text-sm md:text-lg"> Apple Store </p>
                                     </div>
-                                </div>
+                                </a>
                             </div>
                         </div>
-                        <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-800">
-                            <p className="order-2 md:order-1 mt-8 md:mt-0">Recipe App</p>
+                        <div className="mt-20 flex flex-col md:flex-row md:justify-between items-center text-base text-gray-200">
+                            <p className="order-2 md:order-1 mt-8 md:mt-0 px-2">Recipe App</p>
                             <div className="order-1 md:order-2">
-                                <span className="px-2">Acerca de</span>
-                                <span className="px-2 border-l border-black">Contactanos</span>
-                                <span className="px-2 border-l border-black">Políticas de privacidad</span>
+                                <a href="#about" className="px-2 hover:underline">Acerca de</a>
+                                <a href="#contact" className="px-2 hover:underline border-l border-white">Contactanos</a>
+                                <a href="#policy" className="px-2 hover:underline border-l border-white">Políticas de privacidad</a>
                             </div>
                         </div>
                     </div>
